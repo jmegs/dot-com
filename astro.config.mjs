73142d532ccb0bf545b5b58858bf6503955c725e
John Meguerian <johnmeguerian@gmail.com>
2026-04-13 15:11:07 -0400

    Port previous design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Departure Mono",
+    cssVariable: "--font-departure-mono",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/DepartureMono-Regular.woff2'],
+        weight: 'normal',
+        style: 'normal',
+      }],
+    },
+  }],
 });
